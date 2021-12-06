5a35448e5b69f11ce0de39b159cd71c80f960dd8
Marlon <marlon.schnell@web.de>
2021-12-06 05:36:57 +0100

    Added the code for the bot and the needed files for Heroku

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands as cd
+import music
+import os
+
+cogs = [music]
+
+client = cd.Bot(command_prefix="+", intents=discord.Intents.all())
+
+for i in range(len(cogs)):
+    cogs[i].setup(client)
+
+client.run(os.environ("TOKEN"))
---
+++ music.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands as cd
+import youtube_dl as yt
+
+class music(cd.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @cd.command()
+    async def play(self, ctx, url):
+        if ctx.author.voice is None:
+            await ctx.send("Here is James. You're not in a voice channel")
+            return
+        current_channel = ctx.author.voice.channel
+        if ctx.voice_client is None:
+            await current_channel.connect()
+        else:
+            await ctx.voice_client.move_to(current_channel)
+        ctx.voice_client.stop()
+        FFMPEG_OPTIONS = {"before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5", "options" : "-vn"}
+        YDL_OPTIONS = {"format" : "bestaudio"}
+        vc = ctx.voice_client
+
+        with yt.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+            url2 = info["formats"][0]["url"]
+            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+            vc.play(source)
+
+    @cd.command()
+    async def leave(self, ctx):
+        await ctx.voice_client.disconnect()
+
+    @cd.command()
+    async def pause(self, ctx):
+        ctx.voice_client.pause()
+        await ctx.send("This is James, hold position!")
+
+    @cd.command()
+    async def resume(self, ctx):
+        ctx.voice_client.resume()
+        await ctx.send("This is James, ok take over!")
+
+def setup(client):
+    client.add_cog(music(client))
