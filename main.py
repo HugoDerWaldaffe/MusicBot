1d2e7735c25fca55909ec285cecd5600d0d79f5f
Marlon <marlon.schnell@web.de>
2021-12-06 05:43:04 +0100

    Made a change to main.py so the enviromen var should work

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ client = cd.Bot(command_prefix="+", intents=discord.Intents.all())
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-client.run(os.environ("TOKEN"))
+client.run(os.environ["TOKEN"])
